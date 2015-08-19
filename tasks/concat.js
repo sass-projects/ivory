da51044f41c4694ae2dd3a87de5e96f6e04d8701
Daniel Bannert <d.bannert@anolilab.de>
2015-08-19 02:12:27 +0200

    Update regex in concat

--- tasks/concat.js
+++ tasks/concat.js
@@ -9,7 +9,7 @@ var fs = require('fs'),
             'src/scss/grid/*.scss'
     ],
     processFunc = function(src, filepath) {
-        src = src.replace(/(^|\n)[ \t]*(@charset "UTF-8";);?\s*/g, '$1');
+        src = src.replace(/(^|\n)[ \t]*(@charset "UTF-8";|@charset 'UTF-8';);?\s*/g, '$1');
         src = src.replace(/(^|\n)[ \t]*(@import ([^)]*)\;);?\s*/g, '');
 
         return src;
@@ -20,9 +20,7 @@ module.exports = {
         options: {
             // Replace all 'use strict' statements in the code with a single one at the top
             banner: fs.readFileSync('./banner.txt', 'utf8'),
-            process: function(src, filepath) {
-              return src.replace(/(^|\n)[ \t]*(@charset "UTF-8";);?\s*/g, '$1');
-            }
+            process: processFunc
         },
         src: srcFiles,
         dest: 'dist/_ivory.scss',
@@ -31,9 +29,7 @@ module.exports = {
         options: {
             // Replace all 'use strict' statements in the code with a single one at the top
             banner: fs.readFileSync('./banner.txt', 'utf8'),
-            process: function(src, filepath) {
-              return src.replace(/(^|\n)[ \t]*(@charset "UTF-8";);?\s*/g, '$1');
-            }
+            process: processFunc
         },
         src: [
             'node_modules/growcss-sass-config-manager/dist/*.scss',
